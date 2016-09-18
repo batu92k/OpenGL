596be4c0469ce2dd7b30113ae1aa95813c6ff0c8
Batu <batuhan.kindan@gmail.com>
2016-09-18 19:02:28 +0300

    Add files via upload

---
+++ opGlFuncs.h
@@ -0,0 +1,16 @@
+#ifndef OPGLFUNCS_H_INCLUDED
+#define OPGLFUNCS_H_INCLUDED
+
+#include <string>
+
+void initScreen(int, int);
+void dynamicScreen(int , int );
+void resize(int, int );
+void display(void);
+void key(unsigned char, int, int);
+void glMouse(int, int , int , int );
+void idle(void);
+void backgroundColor(double*);
+std::string getExePath();
+
+#endif // OPGLFUNCS_H_INCLUDED
