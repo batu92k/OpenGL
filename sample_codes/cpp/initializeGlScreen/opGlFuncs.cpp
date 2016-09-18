e4528ff9f688eebe2d6543700f120b39c019fdc0
Batu <batuhan.kindan@gmail.com>
2016-09-18 20:36:07 +0300

    Add files via upload

---
+++ sample_codes/cpp/initializeGlScreen/opGlFuncs.cpp
@@ -0,0 +1,65 @@
+#include "opGlFuncs.h"
+#include <Gl/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glext.h>
+#include <windows.h>
+#include <iostream>
+
+void initScreen(int widthInit, int heightInit)
+{
+    std::cout << "initialize window..." << std::endl;
+    glutInitWindowSize(widthInit, heightInit);
+    glutInitWindowPosition(100,100);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );
+    glutCreateWindow("Monitor");
+    std::cout << "window initialization complete..." << std::endl;
+
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(key);
+    glutMouseFunc(glMouse);
+    glutIdleFunc(idle);
+
+    glClearColor(0, 0, 0, 0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glViewport(0, 0, widthInit, heightInit);
+    glOrtho(0 , widthInit, 0, heightInit , -4000, 4000);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutMainLoop();
+}
+
+void resize(int widthRes, int heightRes)
+{
+    glutPostRedisplay();
+}
+
+void display(void)
+{
+    glClearColor(0.24,0.45,0.4,0); // default background color
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    Sleep(1);
+    glutSwapBuffers();
+}
+
+
+void key(unsigned char key, int x, int y)
+{
+    glutPostRedisplay();
+}
+
+void glMouse(int mouseButton, int buttonState, int x, int y)
+{
+    glutPostRedisplay();
+}
+
+void idle(void)
+{
+    glutPostRedisplay();
+}
+
+
---
+++ sample_codes/cpp/initializeGlScreen/opGlFuncs.h
@@ -0,0 +1,13 @@
+#ifndef OPGLFUNCS_H_INCLUDED
+#define OPGLFUNCS_H_INCLUDED
+
+#include <string>
+
+void initScreen(int, int);
+void resize(int, int );
+void display(void);
+void key(unsigned char, int, int);
+void glMouse(int, int , int , int );
+void idle(void);
+
+#endif // OPGLFUNCS_H_INCLUDED
